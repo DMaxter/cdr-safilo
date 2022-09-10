8ab43cc11617d9c44e6f2a1a7efbc2d3d87433a8
DMaxter <daniel_matos@outlook.pt>
2022-09-10 21:00:44 +0100

    feat: endpoint to view roles

--- backend/src/main/kotlin/com/casadosreclamos/dto/UserDto.kt
+++ backend/src/main/kotlin/com/casadosreclamos/dto/UserDto.kt
@@ -1,11 +1,13 @@
 package com.casadosreclamos.dto
 
+import com.casadosreclamos.model.Role
 import com.casadosreclamos.model.User
 
-data class UserDto(val email: String?, val name: String?, val credits: MutableSet<PlafondDto>?) {
+data class UserDto(val email: String?, val name: String?, val roles: MutableSet<Role>, val credits: MutableSet<PlafondDto>?) {
     constructor(user: User) : this(
         user.email,
         user.name,
+        user.roles,
         user.credits.stream().map { PlafondDto(it) }.toList().toMutableSet()
     )
 }
