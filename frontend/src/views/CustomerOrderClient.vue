f0b78ab0b4fc56adf40636eab406500b303b9b8a
DMaxter <daniel_matos@outlook.pt>
2022-11-30 13:06:41 +0000

    feat: search box for users

--- frontend/src/views/CustomerOrderClient.vue
+++ frontend/src/views/CustomerOrderClient.vue
@@ -1,133 +1,125 @@
 <template>
-<v-app>
+  <v-app>
 
-  <v-container fill-height>
-    <v-row justify="center" align="center">
-        <v-col cols="auto" >
+    <v-container fill-height>
+      <v-row justify="center" align="center">
+        <v-col cols="auto">
           <v-card elevation="12" height="600" width="800" tile style="background-color: #E0E0E0">
-          <v-row no-gutters justify="start" class="pt-2 pl-2">
-            <v-menu
-            :offset-x="true"
-            tile
-            >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                height="64"
-                width="100"
-                class="white--text customGradient"
-                tile
-                color="#808080"
-                v-bind="attrs"
-                v-on="on"
-              >
-                Menu
-              </v-btn>
-            </template>
+            <v-row no-gutters justify="start" class="pt-2 pl-2">
+              <v-menu :offset-x="true" tile>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn height="64" width="100" class="white--text customGradient" tile color="#808080" v-bind="attrs"
+                    v-on="on">
+                    Menu
+                  </v-btn>
+                </template>
 
-              <v-btn-toggle v-model="icon" tile dark borderless>
-              <v-btn color="#808080" value="left" height="64" width="160" @click="$router.push('profile')" class="customGradient">
-                  <span class="white--text" style="font-size: 12px">Perfil</span>
+                <v-btn-toggle v-model="icon" tile dark borderless>
+                  <v-btn color="#808080" value="left" height="64" width="160" @click="$router.push('profile')"
+                    class="customGradient">
+                    <span class="white--text" style="font-size: 12px">Perfil</span>
 
-                <v-icon right>
-                  mdi-account-circle
-                </v-icon>
-              </v-btn>
+                    <v-icon right>
+                      mdi-account-circle
+                    </v-icon>
+                  </v-btn>
 
-              <v-btn color="#808080" value="center1" @click="$router.push('history')" height="64" width="160" class="customGradient">
-                <span class="white--text" style="font-size: 12px">Histórico</span>
+                  <v-btn color="#808080" value="center1" @click="$router.push('history')" height="64" width="160"
+                    class="customGradient">
+                    <span class="white--text" style="font-size: 12px">Histórico</span>
 
-                <v-icon right>
-                  mdi-clock
-                </v-icon>
-              </v-btn>
+                    <v-icon right>
+                      mdi-clock
+                    </v-icon>
+                  </v-btn>
 
-              <v-btn color="#808080" value="center2" @click="$router.push('search')" height="64" width="160" class="customGradient">
-                <span class="white--text" style="font-size: 12px">Procurar</span>
+                  <v-btn color="#808080" value="center2" @click="$router.push('search')" height="64" width="160"
+                    class="customGradient">
+                    <span class="white--text" style="font-size: 12px">Procurar</span>
 
-                <v-icon right>
-                  mdi-magnify
-                </v-icon>
-              </v-btn>
+                    <v-icon right>
+                      mdi-magnify
+                    </v-icon>
+                  </v-btn>
 
-              <v-btn color="#808080" value="right" @click="$router.push('orderClient')" height="64" width="160" class="v-btn--active customGradient">
-                <span class="white--text" style="font-size: 12px">Novo Pedido</span>
+                  <v-btn color="#808080" value="right" @click="$router.push('orderClient')" height="64" width="160"
+                    class="v-btn--active customGradient">
+                    <span class="white--text" style="font-size: 12px">Novo Pedido</span>
 
-                <v-icon right>
-                  mdi-playlist-plus
-                </v-icon>
-              </v-btn>
+                    <v-icon right>
+                      mdi-playlist-plus
+                    </v-icon>
+                  </v-btn>
 
-            </v-btn-toggle>
-          </v-menu>
-          </v-row>
+                </v-btn-toggle>
+              </v-menu>
+            </v-row>
             <v-row justify="center" align="center" class="d-flex flex-column mb-11 mt-5">
-              Selecionar cliente e morada de entrega
+              Confirmar a morada de entrega
             </v-row>
             <v-row justify="center" align="center" class="d-flex flex-column mt-2">
-        <v-data-table :headers="headers" :items="desserts2" fixed-header item-key="name" hide-default-footer height="370" style="width: 750px;" class="elevation-1 my-header-style">
-        <template v-slot:[`item.actions`]="{ item }">
-            <v-icon @click="getSpecificClient(item)">mdi-plus</v-icon>
-          </template>
-    </v-data-table>
-     <v-dialog
-      v-model="dialog1"
-      persistent
-      max-width="500px"
-    >
-      <v-card>
-        <v-card-title class="justify-center">
-          <span class="text-h5"> Morada do cliente: </span>
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row justify="center">
-              <span class="text-h6">{{this.selectedClient.address}}, {{this.selectedClient.postalCode}}</span>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions class="justify-center">
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog1 = false; selectedAdress = null"
-          >
-            Voltar
-          </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="nextPage()"
-          >
-            Selecionar
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+              <v-data-table :headers="headers" :items="desserts2" fixed-header disable-pagination
+                :search="dessertFilterValue" tile item-key="name" hide-default-footer height="370" style="width: 750px;"
+                class="elevation-1 my-header-style">
+                <template v-slot:top>
+                  <v-container fluid>
+                    <v-row>
+                      <v-col cols="5">
+                        <v-row class="pa-2">
+                          <v-text-field style="width: 150px;
+                    border-radius: 0px;" v-model="dessertFilterValue" label="Procurar" hide-details outlined
+                            dense></v-text-field>
+                        </v-row>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </template>
+                <template v-slot:[`item.actions`]="{ item }">
+                  <v-icon @click="getSpecificClient(item)">mdi-plus</v-icon>
+                </template>
+              </v-data-table>
+              <v-dialog v-model="dialog1" persistent max-width="500px">
+                <v-card>
+                  <v-card-title class="justify-center">
+                    <span class="text-h5"> Morada do cliente: </span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row justify="center">
+                        <span class="text-h6">{{ this.selectedClient.address }}, {{ this.selectedClient.postalCode }}</span>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions class="justify-center">
+                    <v-btn color="blue darken-1" text @click="dialog1 = false; selectedAdress = null">
+                      Voltar
+                    </v-btn>
+                    <v-btn color="blue darken-1" text @click="nextPage()">
+                      Selecionar
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
             </v-row>
             <v-row no-gutters align="end" justify="space-between" class="d-flex pr-4" style="height: 68px;">
-           <v-col cols="auto" class="pl-4">
-            <v-btn
-              @click="$router.push('profile')"
-              class="d-flex flex-column customGradient"
-              small
-              tile
-              dark
-            > <v-icon style="transform: rotate(180deg);">mdi-play</v-icon>
-            Voltar
-            </v-btn>
-           </v-col>
-            <v-col cols="auto">
-            </v-col>
-           </v-row>
+              <v-col cols="auto" class="pl-4">
+                <v-btn @click="$router.push('profile')" class="d-flex flex-column customGradient" small tile dark>
+                  <v-icon style="transform: rotate(180deg);">mdi-play</v-icon>
+                  Voltar
+                </v-btn>
+              </v-col>
+              <v-col cols="auto">
+              </v-col>
+            </v-row>
           </v-card>
         </v-col>
-    </v-row>
-    <v-row style="position: absolute; bottom: 0px; right: 0px;" class="d-flex"> 
-      <v-img :src="myImage" contain height="180" width="180"></v-img>
-    </v-row>
-    <v-row style="position: absolute; bottom: 20px; right: 20px;" class="d-flex flex-column"> 
-      <span style="font-size: 10px;">© 2022 Casa dos Reclamos, Todos os direitos reservados.</span>
-    </v-row>
+      </v-row>
+      <v-row style="position: absolute; bottom: 0px; right: 0px;" class="d-flex">
+        <v-img :src="myImage" contain height="180" width="180"></v-img>
+      </v-row>
+      <v-row style="position: absolute; bottom: 20px; right: 20px;" class="d-flex flex-column">
+        <span style="font-size: 10px;">© 2022 Casa dos Reclamos, Todos os direitos reservados.</span>
+      </v-row>
     </v-container>
 
   </v-app>
@@ -143,154 +135,154 @@ export default {
   components: {
   },
 
-data () {
-      return {
-        myImage: require('@/assets/logologo1.png'),
-        items: [],
-        selectedAdress: null,
-        allClients: null,
-        desserts2: [],
-        clientName: null,
-        clientEmail: null,
-        clientCode: null,
-        clientPhone: null,
-        clientNIF: null,
-        dialog1: false,
-        store,
-        estadosList: [
-          {text: "Qualquer", value: null},
-          {text: "Sim", value: "sim"},
-          {text: "Não", value: "não"},
-        ],
-        marcasList: [
-          {text: "Qualquer", value: null},
-          {text: "Sim", value: "sim"},
-          {text: "Não", value: "não"},
-        ],
-        // Filter models.
-        dates: [],
-        menu: false,
-        selectedClient: {address: ""},
-      }
-    },
-    async created() { 
-       try {
-        this.allClients = await Backend.getClients()
-        this.allClients.forEach(element => {
-          this.desserts2.push({ name: element.name, fat: element.id })
-        });
-        store.selectedMaterial = []
-        store.currentBrandId = []
-        store.dimensions = []
-        store.images = []
-        store.isActive1 = false
-        store.isActive2 = false
-        store.isActive3 = false
-        store.face1 = null
-        store.face2 = null
-        store.address = null
-        store.uniqueBrands = null
-        store.quantity = null
-        store.observations = null
-        store.currentCost = null
-        store.application = false
-        store.currentClient = null 
-        store.currentAddress = null 
-        store.costPerBrand = null
-        store.pedidoAtual = {
-          cod: null,
-          data: null,
-          marca: null,
-          modelo: null,
-          material: null,
-          dimensoes: null,
-          estado: null,
-          images: null,
-          quantity: null,
-          observations: null,
-          cost: null,
-          application: null
-        },
+  data() {
+    return {
+      myImage: require('@/assets/logologo1.png'),
+      items: [],
+      selectedAdress: null,
+      allClients: null,
+      desserts2: [],
+      clientName: null,
+      clientEmail: null,
+      clientCode: null,
+      clientPhone: null,
+      clientNIF: null,
+      dialog1: false,
+      store,
+      estadosList: [
+        { text: "Qualquer", value: null },
+        { text: "Sim", value: "sim" },
+        { text: "Não", value: "não" },
+      ],
+      marcasList: [
+        { text: "Qualquer", value: null },
+        { text: "Sim", value: "sim" },
+        { text: "Não", value: "não" },
+      ],
+      // Filter models.
+      dates: [],
+      menu: false,
+      selectedClient: { address: "" },
+    }
+  },
+  async created() {
+    try {
+      this.allClients = await Backend.getClients()
+      this.allClients.forEach(element => {
+        this.desserts2.push({ name: element.name, fat: element.id })
+      });
+      store.selectedMaterial = []
+      store.currentBrandId = []
+      store.dimensions = []
+      store.images = []
+      store.isActive1 = false
+      store.isActive2 = false
+      store.isActive3 = false
+      store.face1 = null
+      store.face2 = null
+      store.address = null
+      store.uniqueBrands = null
+      store.quantity = null
+      store.observations = null
+      store.currentCost = null
+      store.application = false
+      store.currentClient = null
+      store.currentAddress = null
+      store.costPerBrand = null
+      store.pedidoAtual = {
+        cod: null,
+        data: null,
+        marca: null,
+        modelo: null,
+        material: null,
+        dimensoes: null,
+        estado: null,
+        images: null,
+        quantity: null,
+        observations: null,
+        cost: null,
+        application: null
+      },
         store.facesDefault = [
-                  {
-                    face: "A",
-                    link: require('@/assets/A.png'),
-                  },
-                        {
-                    face: "B",
-                    link: require('@/assets/B.png'),
-                  },
-                        {
-                    face: "C",
-                    link: require('@/assets/C.png'),
-                  },
-                        {
-                    face: "D",
-                    link: require('@/assets/D.png'),
-                  },
-                        {
-                    face: "E",
-                    link: require('@/assets/E.png'),
-                  },
-                ]
-        console.log(store)
-      } catch (error) {
-        // TODO: Show something
-        console.error(error)
-      }
-    },
-    computed: {
-      headers() {
-        return [
           {
-            text: 'Código',
-            align: 'left',
-            sortable: false,
-            value: 'fat',
-            class: 'my-header-style'
+            face: "A",
+            link: require('@/assets/A.png'),
           },
           {
-            text: 'Nome',
-            value: 'name',
-            align: "center",
-            class: 'my-header-style'
+            face: "B",
+            link: require('@/assets/B.png'),
           },
-          { text: "", value: "actions", align: "right", sortable: false, class: 'my-header-style'},
+          {
+            face: "C",
+            link: require('@/assets/C.png'),
+          },
+          {
+            face: "D",
+            link: require('@/assets/D.png'),
+          },
+          {
+            face: "E",
+            link: require('@/assets/E.png'),
+          },
         ]
-      },
-      
+      console.log(store)
+    } catch (error) {
+      // TODO: Show something
+      console.error(error)
+    }
+  },
+  computed: {
+    headers() {
+      return [
+        {
+          text: 'Código',
+          align: 'left',
+          sortable: false,
+          value: 'fat',
+          class: 'my-header-style'
+        },
+        {
+          text: 'Nome',
+          value: 'name',
+          align: "center",
+          class: 'my-header-style'
+        },
+        { text: "", value: "actions", align: "right", sortable: false, class: 'my-header-style' },
+      ]
     },
-    methods: {
-      getSpecificClient(item){
-        this.allClients.forEach(element => {
-          if(element.name == item.name){
-            this.selectedClient = element
-          }
-        });
-        this.dialog1 = true
-      },
 
-      nextPage(){
-        store.currentAddress = this.selectedClient.address
-        store.currentClient = this.selectedClient.id
-        this.dialog1 = false
-        this.$router.push({name: 'order'});
-      }
+  },
+  methods: {
+    getSpecificClient(item) {
+      this.allClients.forEach(element => {
+        if (element.name == item.name) {
+          this.selectedClient = element
+        }
+      });
+      this.dialog1 = true
+    },
+
+    nextPage() {
+      store.currentAddress = this.selectedClient.address
+      store.currentClient = this.selectedClient.id
+      this.dialog1 = false
+      this.$router.push({ name: 'order' });
     }
   }
+}
 </script>
 
 <style>
-
 #app {
   background: url('@/assets/background.jpg') center center fixed !important;
   background-size: cover;
 }
-  
+
 .my-header-style {
-    border-radius: 0px;
-    background-color: rgb(243, 243, 243) !important;
-  }
+  border-radius: 0px;
+  background-color: rgb(243, 243, 243) !important;
+}
+
 .customGradient {
   background-image: linear-gradient(#616161, grey);
 }
