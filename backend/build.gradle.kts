56d01ccce9b73a50559e8caafa83e513a0f715a9
DMaxter <daniel_matos@outlook.pt>
2022-12-09 12:36:18 +0000

    docs: backend dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -16,24 +16,29 @@ val quarkusPlatformVersion: String by project
 dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation(platform("${quarkusPlatformGroupId}:quarkus-camel-bom:${quarkusPlatformVersion}"))
-    implementation("io.quarkus:quarkus-smallrye-context-propagation")
-    implementation("org.bouncycastle:bcprov-jdk15on")
-    implementation("io.quarkus:quarkus-smallrye-jwt")
-    implementation("io.quarkus:quarkus-smallrye-jwt-build")
-    implementation("io.quarkus:quarkus-security-jpa")
-    implementation("io.quarkus:quarkus-hibernate-reactive-panache")
-    implementation("io.quarkus:quarkus-smallrye-openapi")
-    implementation("io.quarkus:quarkus-kotlin")
-    implementation("io.quarkus:quarkus-reactive-mysql-client")
-    implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
-    implementation("io.quarkus:quarkus-mailer")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    implementation("io.quarkus:quarkus-arc")
-    implementation("io.quarkus:quarkus-resteasy-reactive")
-    implementation("org.apache.camel.quarkus:camel-quarkus-csv")
-    implementation("io.quarkus:quarkus-flyway")
-    implementation("org.flywaydb:flyway-mysql")
-    implementation("io.quarkus:quarkus-jdbc-mysql") // Only to use with flyway
+    implementation("org.jetbrains.kotlin:kotlin-stdlib") // Kotlin
+    implementation("io.quarkus:quarkus-kotlin") // Kotlin for Quarkus
+    implementation("org.bouncycastle:bcprov-jdk15on") // Bouncy Castle Algorithms
+    implementation("io.quarkus:quarkus-smallrye-context-propagation") // Context Propagation for Reactive
+    implementation("io.quarkus:quarkus-security-jpa") // HTTP RBAC
+    implementation("io.quarkus:quarkus-smallrye-openapi") // Swagger UI / OpenAPI
+    implementation("io.quarkus:quarkus-arc") // Context and Dependency Injection
+    implementation("io.quarkus:quarkus-resteasy-reactive-jackson") // REST API Reactive with Jackson serialization
+    implementation("io.quarkus:quarkus-mailer") // SMTP support
+    implementation("org.apache.camel.quarkus:camel-quarkus-csv") // CSV Parser
+
+    // JWT
+    implementation("io.quarkus:quarkus-smallrye-jwt") // Verify JWT tokens
+    implementation("io.quarkus:quarkus-smallrye-jwt-build") // Build JWT tokens
+
+    // Database
+    implementation("io.quarkus:quarkus-hibernate-reactive-panache") // Hibernate Reactive simplified
+    implementation("io.quarkus:quarkus-reactive-mysql-client") // MySQL Reactive client
+    implementation("io.quarkus:quarkus-jdbc-mysql") // MySQL JDBC client (for flyway)
+    implementation("io.quarkus:quarkus-flyway") // Database Migration
+    implementation("org.flywaydb:flyway-mysql") // Flyway MySQL client
+
+    // Testing
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
