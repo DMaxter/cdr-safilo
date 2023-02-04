28e7a43b1c024f4eff2cc816ca51436bffbe4f45
DMaxter <daniel_matos@outlook.pt>
2023-02-04 18:23:33 +0000

    fix: price has set instead of list

--- backend/src/main/kotlin/com/casadosreclamos/dto/PriceDto.kt
+++ backend/src/main/kotlin/com/casadosreclamos/dto/PriceDto.kt
@@ -1,18 +1,19 @@
 package com.casadosreclamos.dto
 
 import com.casadosreclamos.model.request.Price
+import java.util.stream.Collectors
 
 data class PriceDto(
     var id: Long?,
     var material: Long?,
-    var finishings: List<FinishingDto>?,
+    var finishings: Set<FinishingDto>?,
     var costPerSquareMeter: Double?,
     var fixedCost: Double?
 ) {
     constructor(price: Price) : this(
         price.id,
         price.material.id,
-        price.finishings.stream().map { FinishingDto(it) }.toList(),
+        price.finishings.stream().map { FinishingDto(it) }.collect(Collectors.toSet()),
         price.costPerSquareMeter,
         price.fixedCost
     )
