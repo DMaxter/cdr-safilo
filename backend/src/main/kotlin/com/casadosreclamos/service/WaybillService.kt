c6c02d9e6f03ecaa11f004d4d91c7dc5a886f19c
DMaxter <daniel_matos@outlook.pt>
2024-04-04 22:24:26 +0100

    fix: only close day when waybills are created

--- backend/src/main/kotlin/com/casadosreclamos/service/WaybillService.kt
+++ backend/src/main/kotlin/com/casadosreclamos/service/WaybillService.kt
@@ -15,12 +15,17 @@ import io.quarkus.mailer.Mail
 import io.quarkus.mailer.reactive.ReactiveMailer
 import io.quarkus.scheduler.Scheduled
 import io.smallrye.mutiny.Uni
-import org.eclipse.microprofile.config.inject.ConfigProperty
-import org.jboss.logging.Logger
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.LinkOption
+import java.nio.file.attribute.FileTime
+import java.time.Instant
+import java.time.ZoneId
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 import javax.inject.Inject
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.jboss.logging.Logger
 
 private const val CDR_NAME = "Casa dos Reclamos"
 private const val CDR_ADDRESS = "Rua Narciso José Machado Guimarães"
@@ -29,23 +34,19 @@ private const val CDR_CITY = "Vila das Aves"
 private const val CDR_COUNTRY = "PT"
 private const val CDR_PHONE = "+351 252 871 364"
 
+private const val LAST_WAYBILL_FILE = "/safilo/last_waybill"
+
 @ApplicationScoped
 class WaybillService {
-    @Inject
-    lateinit var logger: Logger
+    @Inject lateinit var logger: Logger
 
-    @Inject
-    lateinit var requestService: RequestService
+    @Inject lateinit var requestService: RequestService
 
-    @Inject
-    lateinit var client: FemaClient
+    @Inject lateinit var client: FemaClient
 
-    @Inject
-    lateinit var mailer: ReactiveMailer
+    @Inject lateinit var mailer: ReactiveMailer
 
-    @Inject
-    @ConfigProperty(name = "fema.report.emails")
-    lateinit var emails: List<String>
+    @Inject @ConfigProperty(name = "fema.report.emails") lateinit var emails: List<String>
 
     fun getShippingServices(requestId: Long): Uni<List<Service>> {
         if (requestId <= 0) {
@@ -100,7 +101,13 @@ class WaybillService {
         } else if (waybillDto.description.isNullOrBlank()) {
             logger.error("Description is empty!")
             throw EmptyDescriptionException()
-        } else if (waybillDto.dimensions?.length == null || waybillDto.dimensions.length <= 0 || waybillDto.dimensions.height == null || waybillDto.dimensions.height <= 0 || waybillDto.dimensions.width == null || waybillDto.dimensions.width <= 0) {
+        } else if (waybillDto.dimensions?.length == null ||
+                        waybillDto.dimensions.length <= 0 ||
+                        waybillDto.dimensions.height == null ||
+                        waybillDto.dimensions.height <= 0 ||
+                        waybillDto.dimensions.width == null ||
+                        waybillDto.dimensions.width <= 0
+        ) {
             logger.error("Package dimensions are invalid!")
             throw InvalidMeasurementsException()
         }
@@ -117,26 +124,33 @@ class WaybillService {
                     throw WaybillAlreadyOpenedException()
                 }
 
-                val source = Contact(CDR_NAME, Address(CDR_POSTAL, CDR_COUNTRY, CDR_CITY, CDR_ADDRESS), CDR_PHONE)
+                val source =
+                        Contact(
+                                CDR_NAME,
+                                Address(CDR_POSTAL, CDR_COUNTRY, CDR_CITY, CDR_ADDRESS),
+                                CDR_PHONE
+                        )
 
                 waybillDto.reference = requestId.toString()
                 waybillDto.source = source
 
-                val destination = Contact(
-                    request.client.name,
-                    Address(
-                        request.client.postalCode,
-                        request.client.country,
-                        request.client.city,
-                        request.client.address
-                    ),
-                    request.client.phone
-                )
+                val destination =
+                        Contact(
+                                request.client.name,
+                                Address(
+                                        request.client.postalCode,
+                                        request.client.country,
+                                        request.client.city,
+                                        request.client.address
+                                ),
+                                request.client.phone
+                        )
 
                 // Override default parameters with the ones from request
                 if (waybillDto.destination != null) {
                     if (waybillDto.destination!!.address?.postalCode != null) {
-                        destination.address!!.postalCode = waybillDto.destination!!.address!!.postalCode
+                        destination.address!!.postalCode =
+                                waybillDto.destination!!.address!!.postalCode
                     }
                     if (waybillDto.destination!!.address?.address != null) {
                         destination.address!!.address = waybillDto.destination!!.address!!.address
@@ -156,11 +170,26 @@ class WaybillService {
                 }
 
                 logger.info("Opening waybill at FEMA")
-                client.openWaybill(waybillDto).onItem().transform { label ->
-                    request.trackingCode = label.id
+                client.openWaybill(waybillDto)
+                        .onItem()
+                        .transform { label ->
+                            request.trackingCode = label.id
 
-                    label.file
-                }
+                            label.file
+                        }
+                        .onItem()
+                        .transform { file ->
+                            val date_file = File(LAST_WAYBILL_FILE)
+
+                            date_file.createNewFile()
+
+                            Files.setLastModifiedTime(
+                                    date_file.toPath(),
+                                    FileTime.from(Instant.now())
+                            )
+
+                            file
+                        }
             }
         }
     }
@@ -222,18 +251,39 @@ class WaybillService {
     fun closeDay(): Uni<Void> {
         logger.info("Closing day at FEMA")
 
+        val file = File(LAST_WAYBILL_FILE)
+
+        val modification_date =
+                Files.getLastModifiedTime(file.toPath(), LinkOption.NOFOLLOW_LINKS)
+                        .toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDate()
+        val current_date = Instant.now().atZone(ZoneId.systemDefault()).toLocalDate()
+
+        if (!current_date.isEqual(modification_date)) {
+            logger.info("No waybills opened today")
+            return Uni.createFrom().voidItem()
+        }
+
         return client.closeDay().onItem().transformToUni { report ->
             val currentDate = Calendar.getInstance()
 
             logger.info("Sending email with report")
             return@transformToUni mailer.send(
-                Mail.withText("", "Fecho diário FEMA", getEmailDailyReportSuccess()).addAttachment(
-                    "FEMA-Relatório_${currentDate.get(Calendar.YEAR)}-${currentDate.get(Calendar.MONTH)}-${
+                            Mail.withText("", "Fecho diário FEMA", getEmailDailyReportSuccess())
+                                    .addAttachment(
+                                            "FEMA-Relatório_${currentDate.get(Calendar.YEAR)}-${currentDate.get(Calendar.MONTH)}-${
                         currentDate.get(Calendar.DAY_OF_MONTH)
-                    }.pdf", report.readBytes(), "application/pdf"
-                ).setTo(emails)
-            ).onItem().invoke { _ -> logger.info("Successfully sent email") }.onFailure()
-                .invoke { e -> logger.error("Couldn't send email: $e") }
+                    }.pdf",
+                                            report.readBytes(),
+                                            "application/pdf"
+                                    )
+                                    .setTo(emails)
+                    )
+                    .onItem()
+                    .invoke { _ -> logger.info("Successfully sent email") }
+                    .onFailure()
+                    .invoke { e -> logger.error("Couldn't send email: $e") }
         }
     }
 }
